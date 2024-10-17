5f2a4e1f428a097f58f8692a19603db980b61b67
Rafael Magnago <rmagnago@outlook.com>
2024-10-17 11:41:46 -0300

    feat: remoção do (tabs)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,16 +2,14 @@ import { ScrollView, Text, View } from "react-native";
 
 import Constants from "expo-constants"
 import { Header } from "@/src/components/header";
-import Banner from "@/src/components/banner";
 
 const statusBarHeight = Constants.statusBarHeight;
 
-export default function Index() {
+export default function Home() {
   return (
     <ScrollView style={{ flex: 1 }} className="bg-slate-200" showsVerticalScrollIndicator={false}>
       <View className="w-full" style={{ marginTop: statusBarHeight + 8 }}>
         <Header />
-        <Banner/>
       </View>
 
     </ScrollView>

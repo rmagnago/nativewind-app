5e4e0b0c7f40acd00f18c8b4ac79773fdcf6adeb
Rafael Magnago <rmagnago@outlook.com>
2024-10-17 08:34:24 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
